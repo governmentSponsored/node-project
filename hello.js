f624de4306895e13d6da262977ddb96c720543d3
Stanley Smith <stancsmith@gmail.com>
2016-02-21 20:57:23 -0500

    make server request more standards compliant
    also change port number and add some comments for readability

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 module.exports = {
 	cute: function(str_text) {
-    	console.log('Server running at http://127.0.0.1:1337/ ' + str_text);
+    	console.log('Server running at http://localhost:1337/ ' + str_text);
 	}
 }
--- main.js
+++ main.js
@@ -1,16 +1,20 @@
+//require libraries and files
 var tools = require('./hello'),
 	fs = require('fs'),
-	http = require('http');
+	http = require('http'),
+	server = http.createServer();
 
-tools.cute('carla brady');
+//just messing with console message that gets written
+tools.cute('clabear');
 
+//read contents of index.html and then send it's contents in response to an http request to 1337 port
 fs.readFile('./index.html',function(err, html) {
 	if(err) {
 		throw err;
 	}
-	http.createServer(function(request, response) {  
+	server.on('request', function(request, response) {  
         response.writeHeader(200, {"Content-Type": "text/html"});  
         //response.write();  
         response.end(html);  
-    }).listen(8000);
+    }).listen(1337);
 });
