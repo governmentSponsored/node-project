373b7ba96758f268b436c0b02a85cf0dda7e215b
Stanley Smith <stancsmith@gmail.com>
2016-02-21 20:41:04 -0500

    fix readability and make more efficient
    by removing response.write and passing that info into response.end. also
    not naming the fs library as html.

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 var tools = require('./hello'),
-	html = require('fs'),
+	fs = require('fs'),
 	http = require('http');
 
 tools.cute('carla brady');
 
-html.readFile('./index.html',function(err, html) {
+fs.readFile('./index.html',function(err, html) {
 	if(err) {
 		throw err;
 	}
 	http.createServer(function(request, response) {  
         response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
+        //response.write();  
+        response.end(html);  
     }).listen(8000);
 });
