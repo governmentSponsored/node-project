a66109e2c3818306cceadad56fbe198457b63b00
Stanley Smith <stancsmith@gmail.com>
2016-02-23 13:28:55 -0500

    added second port to listen for pipeable requests

--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
 module.exports = {
 	cute: function(str_text) {
-    	console.log('Server running at http://localhost:1337/ ' + str_text);
+    	console.log('Server running at http://localhost:1337/ ' + str_text + ' \n');
+    	console.log('Server running at http://localhost:1338/. Be sure to curl -d "string" before localhost:1338.');
 	}
 }
--- main.js
+++ main.js
@@ -2,19 +2,30 @@
 var tools = require('./hello'),
 	fs = require('fs'),
 	http = require('http'),
-	server = http.createServer();
+	stream = require('stream');
 
 //just messing with console message that gets written
-tools.cute('clabear');
+tools.cute('clabear!');
+
+//create multiple ports to listen to different requests
+http.createServer(onRequestOneThreeThreeSeven).listen(1337);
+http.createServer(onRequestOneThreeThreeEight).listen(1338);
+
+//working with the stream API here. Just starting off with a quick pipe response to a request.
+
+function onRequestOneThreeThreeSeven (req, res) {
+	res.writeHeader(200);
+	req.pipe(res);
+}
 
 //read contents of index.html and then send it's contents in response to an http request to 1337 port
-fs.readFile('./index.html',function(err, html) {
-	if(err) {
-		throw err;
-	}
-	server.on('request', function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        //response.write();  
-        response.end(html);  
-    }).listen(1337);
-});
+function onRequestOneThreeThreeEight (req, res) {
+	fs.readFile('./index.html', function(err, html) {
+		if(err) {
+			throw err;
+		} else {
+			res.writeHeader(200);
+	        res.end(html);
+		}	        
+	});
+}
