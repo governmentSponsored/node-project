748182ac621982d3284fb1cfcf1a769972eba689
Stanley Smith <stancsmith@gmail.com>
2016-02-23 13:42:58 -0500

    made it easier to update port numbers

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 module.exports = {
-	cute: function(str_text) {
-    	console.log('Server running at http://localhost:1337/ ' + str_text + ' \n');
-    	console.log('Server running at http://localhost:1338/. Be sure to curl -d "string" before localhost:1338.');
+	cute: function(options) {
+    	console.log('Server running at http://localhost:' + options.portOne + '/ \n');
+    	console.log('Server running at http://localhost:' + options.portTwo + '/. Be sure to curl -d "string"');
 	}
 }
--- main.js
+++ main.js
@@ -2,23 +2,25 @@
 var tools = require('./hello'),
 	fs = require('fs'),
 	http = require('http'),
-	stream = require('stream');
+	stream = require('stream'),
+	options = {portOne: 1337, portTwo: 1338};
 
 //just messing with console message that gets written
-tools.cute('clabear!');
+tools.cute(options);
 
 //create multiple ports to listen to different requests
-http.createServer(onRequestOneThreeThreeSeven).listen(1337);
-http.createServer(onRequestOneThreeThreeEight).listen(1338);
+http.createServer(onRequestOneThreeThreeSeven).listen(options.portOne);
+http.createServer(onRequestOneThreeThreeEight).listen(options.portTwo);
 
 //working with the stream API here. Just starting off with a quick pipe response to a request.
-
+//here is where you use: curl -d "string" http://localhost:1337
 function onRequestOneThreeThreeSeven (req, res) {
 	res.writeHeader(200);
 	req.pipe(res);
 }
 
 //read contents of index.html and then send it's contents in response to an http request to 1337 port
+//here you use the normal curl http://localhost:1338
 function onRequestOneThreeThreeEight (req, res) {
 	fs.readFile('./index.html', function(err, html) {
 		if(err) {
